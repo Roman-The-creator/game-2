b887043347e8d56e4145d613f1e6bf5d20ca970e
TyrelKO <sukhininartem70@gmail.com>
2025-05-21 16:49:40 +0500

    Добавлены таймер и мигание при перегреве

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -28,50 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.btnCoolDown = new System.Windows.Forms.Button();
+            this.labelBalance = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.numericStake = new System.Windows.Forms.NumericUpDown();
+            this.trackEnergy = new System.Windows.Forms.TrackBar();
+            this.trackSpeed = new System.Windows.Forms.TrackBar();
+            this.trackVoltage = new System.Windows.Forms.TrackBar();
+            this.progressTemperature = new System.Windows.Forms.ProgressBar();
+            this.btnRunReaction = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericStake)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackEnergy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackVoltage)).BeginInit();
             this.SuspendLayout();
+            
             // 
-            // label1
+            // btnCoolDown
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(135, 105);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.btnCoolDown.Location = new System.Drawing.Point(204, 66);
+            this.btnCoolDown.Name = "btnCoolDown";
+            this.btnCoolDown.Size = new System.Drawing.Size(182, 23);
+            this.btnCoolDown.TabIndex = 3;
+            this.btnCoolDown.Text = "Охладить реактор";
+            this.btnCoolDown.UseVisualStyleBackColor = true;
+            this.btnCoolDown.Click += new System.EventHandler(this.btnCoolDown_Click);
             // 
-            // label2
+            // labelBalance
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(135, 211);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "labelResult";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.labelBalance.AutoSize = true;
+            this.labelBalance.Location = new System.Drawing.Point(633, 184);
+            this.labelBalance.Name = "labelBalance";
+            this.labelBalance.Size = new System.Drawing.Size(44, 16);
+            this.labelBalance.TabIndex = 5;
+            this.labelBalance.Text = "label5";
             // 
-            // label3
+            // labelResult
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(270, 168);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 16);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "labelBalance";
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(775, 220);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(44, 16);
+            this.labelResult.TabIndex = 6;
+            this.labelResult.Text = "label5";
+            // 
+            // numericStake
+            // 
+            this.numericStake.Location = new System.Drawing.Point(563, 298);
+            this.numericStake.Name = "numericStake";
+            this.numericStake.Size = new System.Drawing.Size(120, 22);
+            this.numericStake.TabIndex = 7;
+            // 
+            // trackEnergy
+            // 
+            this.trackEnergy.Location = new System.Drawing.Point(538, 105);
+            this.trackEnergy.Name = "trackEnergy";
+            this.trackEnergy.Size = new System.Drawing.Size(104, 56);
+            this.trackEnergy.TabIndex = 8;
+            // 
+            // trackSpeed
+            // 
+            this.trackSpeed.Location = new System.Drawing.Point(988, 165);
+            this.trackSpeed.Name = "trackSpeed";
+            this.trackSpeed.Size = new System.Drawing.Size(104, 56);
+            this.trackSpeed.TabIndex = 9;
+            // 
+            // trackVoltage
+            // 
+            this.trackVoltage.Location = new System.Drawing.Point(952, 277);
+            this.trackVoltage.Name = "trackVoltage";
+            this.trackVoltage.Size = new System.Drawing.Size(168, 56);
+            this.trackVoltage.TabIndex = 10;
+            // 
+            // progressTemperature
+            // 
+            this.progressTemperature.Location = new System.Drawing.Point(441, 395);
+            this.progressTemperature.Name = "progressTemperature";
+            this.progressTemperature.Size = new System.Drawing.Size(122, 22);
+            this.progressTemperature.TabIndex = 11;
+            // 
+            // btnRunReaction
+            // 
+            this.btnRunReaction.Location = new System.Drawing.Point(375, 180);
+            this.btnRunReaction.Name = "btnRunReaction";
+            this.btnRunReaction.Size = new System.Drawing.Size(75, 23);
+            this.btnRunReaction.TabIndex = 12;
+            this.btnRunReaction.Text = "button1";
+            this.btnRunReaction.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1686, 669);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRunReaction);
+            this.Controls.Add(this.progressTemperature);
+            this.Controls.Add(this.trackVoltage);
+            this.Controls.Add(this.trackSpeed);
+            this.Controls.Add(this.trackEnergy);
+            this.Controls.Add(this.numericStake);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.labelBalance);
+            this.Controls.Add(this.btnCoolDown);
+            
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericStake)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackEnergy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackVoltage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,8 +145,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCoolDown;
+        private System.Windows.Forms.Label labelBalance;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.NumericUpDown numericStake;
+        private System.Windows.Forms.TrackBar trackEnergy;
+        private System.Windows.Forms.TrackBar trackSpeed;
+        private System.Windows.Forms.TrackBar trackVoltage;
+        private System.Windows.Forms.ProgressBar progressTemperature;
+        private System.Windows.Forms.Button btnRunReaction;
     }
 }
--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -10,16 +10,35 @@ namespace my_game.Forms
         private GameState _state;
         private ReactorEngine _engine;
 
+        private Timer _blinkTimer;
+        private bool _isBlinkVisible = true;
+
+        private Timer _gameTimer;
+        private TimeSpan _remainingTime = TimeSpan.FromMinutes(5);
+
         public MainForm()
         {
             InitializeComponent();
+
             _state = new GameState();
             _engine = new ReactorEngine();
+
+            // Таймер мигания при перегреве
+            _blinkTimer = new Timer();
+            _blinkTimer.Interval = 500;
+            _blinkTimer.Tick += BlinkTimer_Tick;
+            _blinkTimer.Start();
+
+            // Таймер обратного отсчета игры
+            _gameTimer = new Timer();
+            _gameTimer.Interval = 1000;
+            _gameTimer.Tick += GameTimer_Tick;
+            _gameTimer.Start();
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            // Установка начальных значений
+            // Настройка параметров элементов управления
             trackEnergy.Minimum = 0;
             trackEnergy.Maximum = 100;
             trackEnergy.Value = 50;
@@ -46,6 +65,15 @@ namespace my_game.Forms
         {
             labelBalance.Text = $"Баланс: {_state.Balance} фишек";
             progressTemperature.Value = Math.Min(progressTemperature.Maximum, _state.Temperature);
+
+            if (_state.Temperature >= 90)
+            {
+                labelResult.Text = "⚠ Реактор перегревается!";
+            }
+            else
+            {
+                labelResult.ForeColor = System.Drawing.Color.Black;
+            }
         }
 
         private void btnRunReaction_Click(object sender, EventArgs e)
@@ -61,14 +89,52 @@ namespace my_game.Forms
             UpdateUI();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void btnCoolDown_Click(object sender, EventArgs e)
         {
+            if (_state.Balance >= 2)
+            {
+                _state.Balance -= 2;
+                _state.Temperature = Math.Max(0, _state.Temperature - 25);
+                labelResult.Text = "Реактор охлаждён!";
+            }
+            else
+            {
+                labelResult.Text = "Недостаточно фишек для охлаждения!";
+            }
 
+            UpdateUI();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void BlinkTimer_Tick(object sender, EventArgs e)
         {
+            if (_state.Temperature >= 90)
+            {
+                _isBlinkVisible = !_isBlinkVisible;
+                labelResult.ForeColor = _isBlinkVisible ? System.Drawing.Color.Red : System.Drawing.Color.Black;
+            }
+            else
+            {
+                labelResult.ForeColor = System.Drawing.Color.Black;
+                _isBlinkVisible = true;
+            }
+        }
 
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            _remainingTime = _remainingTime.Subtract(TimeSpan.FromSeconds(1));
+            this.Text = $"Fortune Reactor — осталось: {_remainingTime:mm\\:ss}";
+
+            if (_remainingTime <= TimeSpan.Zero)
+            {
+                _gameTimer.Stop();
+                _blinkTimer.Stop();
+
+                MessageBox.Show("Время вышло! Игра завершена.", "Итоги", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                btnRunReaction.Enabled = false;
+                btnCoolDown.Enabled = false;
+            }
         }
     }
 }
+
